cadf7b126df1faff9eeea3a24a791de7ce0e0152
sebastianx9407x <sebastianx9407x@gmail.com>
2024-07-30 21:26:23 -0400

    restructuring

---
+++ Mappers/Mapper.cpp
@@ -0,0 +1,38 @@
+
+#include "Mapper.h"
+
+Mapper::Mapper(uint8_t prgBanks, uint8_t chrBanks)
+{
+	nPRGBanks = prgBanks;
+	nCHRBanks = chrBanks;
+
+	reset();
+}
+
+
+Mapper::~Mapper()
+{
+}
+
+void Mapper::reset()
+{
+
+}
+
+MIRROR Mapper::mirror()
+{
+	return MIRROR::HARDWARE;
+}
+
+bool Mapper::irqState()
+{
+	return false;
+}
+
+void Mapper::irqClear()
+{
+}
+
+void Mapper::scanline()
+{
+}
